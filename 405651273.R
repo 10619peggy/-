dc2eb90fdf5065d49facc10f85f5e868236b23e9
10619peggy <56258192+10619peggy@users.noreply.github.com>
2019-10-08 19:51:05 +0800

    Add files via upload
    資料探勘作業

---
+++ 405651273.R
@@ -0,0 +1,27 @@
+﻿people = read.csv("d:/jyCharacters.csv")
+
+dim(people)
+names(people)
+head(people,3)	# tail(people,5) 看最後五列
+
+mean(people$age)		# median, sd, var, sum, .....
+summary(people$luck)
+table(people$gender)
+table(people$rank)
+table(people$ending)
+stem(people$luck)		# 枝葉圖 (stem and leaf)
+# histogram 直方圖
+hist(people$age,col=rainbow(15),xlab="年齡",ylab="人數")
+# 長條圖
+barplot(table(people$rank),col=rainbow(7))
+pie(table(people$rank),col=rainbow(12),main="角色比例")
+boxplot(property ~ gender,data=people,col=rainbow(12))
+
+# 哪些因素決定一個角色的重要性(rank) ?
+people2 = subset(people,select=-c(name,note)) # 去除姓名跟備註
+people2$rank = ordered(people2$rank)		# 轉為順序型分類變數
+library(rpart)			# 載入(load) rpart 套件, 只需做一次
+result = rpart(rank~.,data=people2,control=rpart.control(minsplit = 5))
+plot(result);text(result)
+result
+
